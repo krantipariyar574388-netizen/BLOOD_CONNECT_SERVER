b20a9e278e4c97c3ea3da5810e92e3f74fb9423b
krantipariyar574388-netizen <krantipariyar574388@gmail.com>
2026-08-20 21:16:46 +0545

    add : cors wildcard

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { AppError } from "./utils/customError.util";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import notificationRoutes from './routes/notification.route';
+import cors from 'cors';
 
 const app = express();
 
@@ -13,6 +14,10 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin : "*",
+}),
+);
 
 app.get("/",(req : Request, res : Response) => {
     res.status(200).json({
@@ -36,3 +41,5 @@ app.use((req, res, next) => {
 app.use(errorHandler);
 
 export default app;
+
+// 23 july 58:57
